f47f558c3ec6c068e26f95e8196634ead9a85489
xd963741 <xd963741@gmail.com>
2025-07-02 19:20:40 +0800

    Fix Dockerfile path for Render deployment

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
+# Build stage
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 WORKDIR /src
-COPY ./dotnet/LineNotifierApi/ ./
+COPY . .
 RUN dotnet publish -c Release -o /app
 
+# Runtime stage
 FROM mcr.microsoft.com/dotnet/aspnet:8.0
 WORKDIR /app
 COPY --from=build /app .
--- obj/Debug/net9.0/LineNotifierApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/LineNotifierApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LineNotifierApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9513fd91f72ce04449c5f5be055113734fb156a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LineNotifierApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LineNotifierApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
